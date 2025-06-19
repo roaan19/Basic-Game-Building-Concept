eef2829a593104379226e7ac2a0e7d419756bb5d
roaan19 <141211644+roaan19@users.noreply.github.com>
2025-06-19 15:41:08 +0530

    Add files via upload

--- intro.py
+++ intro.py
@@ -1,10 +1,14 @@
-import pygame 
+import pygame
 pygame.init()
-screen=pygame.display.set_mode((400,300))
-done=False
-while not done :
+window=pygame.display.set_mode((400,400))
+window.fill((255,255,255))
+GREEN = (0,225,0)
+pygame.draw.circle(window,GREEN,(300,300),50)
+pygame.draw.circle(window,GREEN,(100,100),50,3)
+pygame.display.update()
+running=True
+while running:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT :
-            done=True
-    pygame.draw.rect(screen, (0,125,255),pygame.Rect(30,30,60,60))
-    pygame.display.flip()
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
